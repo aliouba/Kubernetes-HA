c92bde4850fea8ca9466d41ce9f2f9035fe2a8a8
BA Aliou <alioubafr@gmail.com>
2017-05-05 21:25:21 +0200

    Update kubelet.sh

--- kube-tls/kubelet.sh
+++ kube-tls/kubelet.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 #
-# Invoke this command with ./create-tls-assets-for-worker [ WORKER_ID ] [ WORKER_IP ]
+# Invoke this command with ./create-tls-assets-for-worker [ workerID ] [ WorkerIP ]
 
 # The worker ID should be an integer,
-# and will place TLS assets in `/etc/kubenetes/ssl/worker${WORKER_ID}/`
-WORKER_ID=$1
+# and will place TLS assets in `/etc/kubenetes/ssl/worker${workerID}/`
+workerID=$1
 
 # The private IP of the worker node.
-WORKER_IP=$2
+WorkerIP=$2
 
-mkdir -p /etc/kubernetes/ssl/worker${WORKER_ID};
+mkdir -p /etc/kubernetes/ssl/worker${workerID};
 
-cp ca.pem /etc/kubernetes/ssl/worker${WORKER_ID};
+cp ca.pem /etc/kubernetes/ssl/worker${workerID};
 
-cp ca-key.pem /etc/kubernetes/ssl/worker${WORKER_ID};
+cp ca-key.pem /etc/kubernetes/ssl/worker${workerID};
 
-cp worker-openssl.cnf /etc/kubernetes/ssl/worker${WORKER_ID};
+cp worker-openssl.cnf /etc/kubernetes/ssl/worker${workerID};
 
 # Place the actual api server hostname in the open ssl config file.
-sed -i -e "s/WORKER_IP/${WORKER_IP}/g" /etc/kubernetes/ssl/worker${WORKER_ID}/worker-openssl.cnf;
+sed -i -e "s/WorkerIP/${WorkerIP}/g" /etc/kubernetes/ssl/worker${workerID}/worker-openssl.cnf;
 
-openssl genrsa -out /etc/kubernetes/ssl/worker${WORKER_ID}/worker-key.pem 2048
+openssl genrsa -out /etc/kubernetes/ssl/worker${workerID}/worker-key.pem 2048
 
-WORKER_IP=${WORKER_IP} openssl req -new \
-  -key /etc/kubernetes/ssl/worker${WORKER_ID}/worker-key.pem \
-	-out /etc/kubernetes/ssl/worker${WORKER_ID}/worker.csr \
-	-subj "/CN=worker${WORKER_ID}" \
-	-config /etc/kubernetes/ssl/worker${WORKER_ID}/worker-openssl.cnf;
+WorkerIP=${WorkerIP} openssl req -new \
+  -key /etc/kubernetes/ssl/worker${workerID}/worker-key.pem \
+	-out /etc/kubernetes/ssl/worker${workerID}/worker.csr \
+	-subj "/CN=worker${workerID}" \
+	-config /etc/kubernetes/ssl/worker${workerID}/worker-openssl.cnf;
 
-WORKER_IP=${WORKER_IP} openssl x509 -req \
-  -in /etc/kubernetes/ssl/worker${WORKER_ID}/worker.csr \
-	-CA /etc/kubernetes/ssl/worker${WORKER_ID}/ca.pem \
-	-CAkey /etc/kubernetes/ssl/worker${WORKER_ID}/ca-key.pem \
+WorkerIP=${WorkerIP} openssl x509 -req \
+  -in /etc/kubernetes/ssl/worker${workerID}/worker.csr \
+	-CA /etc/kubernetes/ssl/worker${workerID}/ca.pem \
+	-CAkey /etc/kubernetes/ssl/worker${workerID}/ca-key.pem \
 	-CAcreateserial \
-	-out /etc/kubernetes/ssl/worker${WORKER_ID}/worker.pem \
+	-out /etc/kubernetes/ssl/worker${workerID}/worker.pem \
 	-days 365 \
 	-extensions v3_req \
-	-extfile /etc/kubernetes/ssl/worker${WORKER_ID}/worker-openssl.cnf;
+	-extfile /etc/kubernetes/ssl/worker${workerID}/worker-openssl.cnf;
 
-sudo chmod 600 /etc/kubernetes/ssl/worker${WORKER_ID}/*-key.pem
-sudo chown root:root /etc/kubernetes/ssl/worker${WORKER_ID}/*-key.pem
+sudo chmod 600 /etc/kubernetes/ssl/worker${workerID}/*-key.pem
+sudo chown root:root /etc/kubernetes/ssl/worker${workerID}/*-key.pem
